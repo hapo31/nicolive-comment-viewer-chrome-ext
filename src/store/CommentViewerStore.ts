7707818a3b4f180d3051240734327c904a4d9007
happo31 <happo31@gmail.com>
2018-05-12 04:10:17 +0900

    コメント受診時のハンドラを登録できるように

--- src/store/CommentViewerStore.ts
+++ src/store/CommentViewerStore.ts
@@ -1,16 +1,21 @@
 import { observable, action, computed } from "mobx";
-import { ChatData } from "../model/Chat";
+import { Chat, ChatData } from "../model/Chat";
 import CommentServerClient from "../model/CommentServerClient";
 import LiveGetPlayerStatusClient from "../model/LiveGetPlayerStatusClient";
 import nicoLiveData from "../model/NicoLiveData";
 import ICommentServerClient from "../model/ICommentServerClient";
 import ThreadStore from "./ThreadStore";
 
+export type OnReceiveHanlder = (chatData: Partial<Chat>) => void;
+
 export default class CommentViewerStore {
   @observable private threads: ThreadStore[] = [];
+  @observable public appStartDate: Date = new Date();
   @observable public isBroadcaster: boolean = true;
 
   public commentServerClients: ICommentServerClient[] = [];
+
+  private onReceiveHandlers: OnReceiveHanlder[] = [];
 
   constructor(prop?: Partial<CommentViewerStore>) {
     if (prop) {
@@ -40,14 +45,36 @@ export default class CommentViewerStore {
         this.commentServerClients.forEach(v => {
           // 接続しに行く
           // コールバックの登録もする
-          v.connect(chatData => this.onReceiveChat(chatData));
+          v.connect(chatData => {
+            const chat = this.pushChatData(chatData);
+            // 拡張機能が起動したあとに受信したコメントのみハンドリングする
+            if (this.appStartDate <= chat.date) {
+              this.onReceiveHandlers.forEach(handler => {
+                // コールバックの呼び出し
+                handler(chat);
+              });
+            }
+          });
         });
       }
     }
   }
 
+  public addOnReceiveHandler(handler: OnReceiveHanlder) {
+    this.onReceiveHandlers.push(handler);
+  }
+
+  public removeOnReceiveHandler(handler: OnReceiveHanlder) {
+    const index = this.onReceiveHandlers.findIndex(
+      instance => instance === handler
+    );
+    if (index >= 0) {
+      this.onReceiveHandlers.splice(index, 1);
+    }
+  }
+
   @action.bound
-  private onReceiveChat(chatData: ChatData) {
+  private pushChatData(chatData: ChatData) {
     // 受信したデータと一致するスレッドIDをメンバのThreadオブジェクトリストから探す
     const threadIndex = this.threads.findIndex(
       v => v.threadId === chatData.thread
@@ -56,11 +83,13 @@ export default class CommentViewerStore {
       // 受信データを内部に追加
       const thread = this.threads[threadIndex];
       thread.pushChatData(chatData);
+      return thread.lastChat!;
     } else {
       // 見つからなかった場合は新たにスレッドを作成
       const thread = new ThreadStore({ threadId: chatData.thread });
       thread.pushChatData(chatData);
       this.threads.push(thread);
+      return thread.lastChat!;
     }
   }
 
