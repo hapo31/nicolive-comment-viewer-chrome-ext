0a6021e49619f8d1f553210be896c018b18ab674
happo31 <happo31@gmail.com>
2018-05-14 00:59:20 +0900

    パフォーマンス向上

--- src/store/ThreadStore.ts
+++ src/store/ThreadStore.ts
@@ -1,8 +1,12 @@
 import { observable, action } from "mobx";
 import { Chat, ChatData } from "../model/Chat";
+
 export default class ThreadStore {
   @observable public threadId: number = 0;
   @observable public chatList: Chat[] = [];
+
+  private chatListCache: Chat[] = [];
+  private timer: number = 0;
 
   constructor(props?: Partial<ThreadStore>) {
     if (props) {
@@ -12,14 +16,25 @@ export default class ThreadStore {
 
   @action.bound
   public pushChatData(data: ChatData) {
-    this.chatList.push(new Chat(data));
-    // 追加のたびにソートするのはパフォーマンスまずそう
-    this.chatList.sort((a, b) => a.commentNo - b.commentNo);
+    this.chatListCache.push(new Chat(data));
+
+    if (this.timer !== 0) {
+      clearTimeout(this.timer);
+    }
+    // 起動時などに一気にコメントが追加されると激重になるので、
+    // 自前でデータへの反映にthrottleを掛ける
+    this.timer = window.setTimeout(() => {
+      this.chatList = this.chatList
+        .concat(this.chatListCache)
+        .sort((a, b) => b.commentNo - a.commentNo);
+      this.chatListCache = [];
+      this.timer = 0;
+    }, 500);
   }
 
   public get lastChat() {
-    return this.chatList.length !== 0
-      ? this.chatList[this.chatList.length - 1]
+    return this.chatListCache.length !== 0
+      ? this.chatListCache[this.chatListCache.length - 1]
       : null;
   }
 }
