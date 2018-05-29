a31c504a8b6a2230e58df891fa245a7bbb75c74c
happo31 <happo31@gmail.com>
2018-05-30 03:56:50 +0900

    fix: 変数名

--- src/loader.ts
+++ src/loader.ts
@@ -1,11 +1,14 @@
 // ページのwindowオブジェクトを取得するため、拡張機能のスクリプトを読み込むscriptタグをページに埋め込む
 if (chrome) {
-  const scriptName = "inject.js";
-  const scriptElem = document.createElement("script");
+  const injectScriptName = "inject.js";
+  const injectScriptElem = document.createElement("script");
   const scriptURL = chrome.extension.getURL("script.js");
 
-  scriptElem.setAttribute("id", "nicolive-commentviewer-extenstion");
-  scriptElem.setAttribute("src", chrome.extension.getURL(scriptName));
-  scriptElem.setAttribute("data-script-url", scriptURL);
-  document.documentElement.appendChild(scriptElem);
+  injectScriptElem.setAttribute("id", "nicolive-commentviewer-extenstion");
+  injectScriptElem.setAttribute(
+    "src",
+    chrome.extension.getURL(injectScriptName)
+  );
+  injectScriptElem.setAttribute("data-script-url", scriptURL);
+  document.documentElement.appendChild(injectScriptElem);
 }
